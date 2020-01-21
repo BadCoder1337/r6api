3a1b4146d4382c4a0fb718260a66ae503adeabcb
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-01-21 19:04:11 +0300

    New login URL

--- src/constants.ts
+++ src/constants.ts
@@ -27,7 +27,7 @@ export const URLS: { [key: string]: { [key: string]: string } } = {
             "https://public-ubiservices.ubi.com/v1/spaces/5172a557-50b5-4665-b7db-e3f2e8c5041d/sandboxes/OSBOR_PC_LNCH_A/playerstats2/statistics?statistics=casualpvp_timeplayed,rankedpvp_timeplayed&populations=",
         RANK_URL:
             "https://public-ubiservices.ubi.com/v1/spaces/5172a557-50b5-4665-b7db-e3f2e8c5041d/sandboxes/OSBOR_PC_LNCH_A/r6karma/players?board_id=pvp_ranked&",
-        LOGIN_URL: "https://uplayconnect.ubi.com/ubiservices/v2/profiles/sessions?"
+        LOGIN_URL: "https://public-ubiservices.ubi.com/v3/profiles/sessions?"
     },
     "PS4": {
         URL:
@@ -42,7 +42,7 @@ export const URLS: { [key: string]: { [key: string]: string } } = {
             "https://public-ubiservices.ubi.com/v1/spaces/05bfb3f7-6c21-4c42-be1f-97a33fb5cf66/sandboxes/OSBOR_PS4_LNCH_A/playerstats2/statistics?statistics=casualpvp_timeplayed,rankedpvp_timeplayed&populations=",
         RANK_URL:
             "https://public-ubiservices.ubi.com/v1/spaces/05bfb3f7-6c21-4c42-be1f-97a33fb5cf66/sandboxes/OSBOR_PS4_LNCH_A/r6karma/players?board_id=pvp_ranked&",
-        LOGIN_URL: "https://uplayconnect.ubi.com/ubiservices/v2/profiles/sessions?"
+        LOGIN_URL: "https://public-ubiservices.ubi.com/v3/profiles/sessions?"
     },
     "XBOX": {
         URL:
@@ -57,7 +57,7 @@ export const URLS: { [key: string]: { [key: string]: string } } = {
             "https://public-ubiservices.ubi.com/v1/spaces/98a601e5-ca91-4440-b1c5-753f601a2c90/sandboxes/OSBOR_XBOXONE_LNCH_A/playerstats2/statistics?statistics=casualpvp_timeplayed,rankedpvp_timeplayed&populations=",
         RANK_URL:
             "https://public-ubiservices.ubi.com/v1/spaces/98a601e5-ca91-4440-b1c5-753f601a2c90/sandboxes/OSBOR_XBOXONE_LNCH_A/r6karma/players?board_id=pvp_ranked&",
-        LOGIN_URL: "https://uplayconnect.ubi.com/ubiservices/v2/profiles/sessions?"
+        LOGIN_URL: "https://public-ubiservices.ubi.com/v3/profiles/sessions?"
     }
 }
 
